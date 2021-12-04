bc1e6667cf131c4a8a570288f0a1a9f51276bb58
Kacper <thestahan@gmail.com>
2021-12-04 13:14:07 +0100

    Day 4 solution

---
+++ Advent of Code 2021/4_giant_squid/Board.cs
@@ -0,0 +1,43 @@
+﻿namespace _4_giant_squid
+{
+    internal class Board
+    {
+        public List<List<int>> RowsData { get; set; } = new List<List<int>>();
+
+        public void MarkBingoNumber(int number)
+        {
+            for (int i = 0; i < RowsData.Count; i++)
+            {
+                for (int j = 0; j < RowsData[i].Count; j++)
+                {
+                    if (RowsData[i][j] == number) RowsData[i][j] = -1;
+                }
+            }
+        }
+
+        public bool DidWin()
+        {
+            for (int i = 0; i < RowsData.Count; i++)
+            {
+                bool rowWon = RowsData[i].All(x => x == -1);
+
+                List<int> columnData = new();
+
+                for (int j = 0; j < RowsData[i].Count; j++) columnData.Add(RowsData[j][i]);
+
+                bool columnWon = columnData.All(x => x == -1);
+
+                if (columnWon || rowWon) return true;
+            }
+
+            return false;
+        }
+
+        public int GetWinningScore(int lastNumber)
+        {
+            int remainingNumbersSum = RowsData.Select(r => r.Where(c => c != -1).Sum(c => c)).Sum();
+
+            return remainingNumbersSum * lastNumber;
+        }
+    }
+}
---
+++ Advent of Code 2021/4_giant_squid/Program.cs
@@ -0,0 +1,73 @@
+﻿using _4_giant_squid;
+
+var inputData = File.ReadAllLines("input.txt");
+
+var moves = inputData.First().Split(",").Select(x => Int32.Parse(x));
+
+var boards = new List<Board>();
+
+for (int i = 2; i < inputData.Length; i += 6)
+{
+    var rowsData = new List<List<int>>
+    {
+        inputData[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => Int32.Parse(x)).ToList(),
+        inputData[i + 1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => Int32.Parse(x)).ToList(),
+        inputData[i + 2].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => Int32.Parse(x)).ToList(),
+        inputData[i + 3].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => Int32.Parse(x)).ToList(),
+        inputData[i + 4].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => Int32.Parse(x)).ToList()
+    };
+
+    boards.Add(new Board { RowsData = rowsData });
+}
+
+int winningScore = SimulateGamePartOne(moves, boards);
+
+int lastWinningScore = SimulateGamePartTwo(moves, boards);
+
+Console.WriteLine("First part: " + winningScore);
+
+Console.WriteLine("Second part: " + lastWinningScore);
+
+static int SimulateGamePartOne(IEnumerable<int> moves, List<Board> boards)
+{
+    foreach (var move in moves)
+    {
+        foreach (var board in boards)
+        {
+            board.MarkBingoNumber(move);
+
+            if (board.DidWin()) return board.GetWinningScore(move);
+        }
+    }
+
+    return 0;
+}
+
+static int SimulateGamePartTwo(IEnumerable<int> moves, List<Board> boards)
+{
+    var indexesToConsider = Enumerable.Range(0, boards.Count).ToList();
+
+    foreach (var move in moves)
+    {
+        for (int i = 0; i < boards.Count; i++)
+        {
+            if (!indexesToConsider.Contains(i)) continue;
+
+            var board = boards[i];
+
+            board.MarkBingoNumber(move);
+
+            if (board.DidWin())
+            {
+                if (indexesToConsider.Count == 1)
+                {
+                    return board.GetWinningScore(move);
+                }
+
+                indexesToConsider.Remove(i);
+            }
+        }
+    }
+
+    return 0;
+}
