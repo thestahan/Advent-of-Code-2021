9c0f11aec4b6110d895bc2008b085be3531c0eec
Kacper <thestahan@gmail.com>
2021-12-04 13:07:47 +0100

    Day 2 solution

---
+++ Advent of Code 2021/2_dive/Program.cs
@@ -0,0 +1,34 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+int depth = 0;
+
+int horizontal = 0;
+
+int aim = 0;
+
+foreach (var line in input)
+{
+    var splittedLine = line.Split(" ");
+
+    string actionName = splittedLine[0];
+
+    int value = int.Parse(splittedLine[1]);
+
+    if (actionName == "forward")
+    {
+        horizontal += value;
+        depth += value * aim;
+    }
+    else if (actionName == "up")
+    {
+        //depth -= value;
+        aim -= value;
+    }
+    else
+    {
+        //depth += value;
+        aim += value;
+    }
+}
+
+Console.WriteLine(depth + " * " + horizontal + " = " + depth * horizontal);
