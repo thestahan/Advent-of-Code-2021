ca4f580d7246fe763330e5c6a79b38c931302c78
Kacper <thestahan@gmail.com>
2021-12-04 13:03:40 +0100

    Day 1 solution

---
+++ Advent of Code 2021/1_sonar_sweep/Program.cs
@@ -0,0 +1,33 @@
+﻿string path = Path.Combine(Environment.CurrentDirectory, "input.txt");
+
+var inputData = File.ReadAllLines(path);
+
+int counter = 0;
+
+int prev = Convert.ToInt32(inputData[0]);
+
+foreach (var data in inputData.Skip(1))
+{
+    int value = Convert.ToInt32(data);
+
+    if (value > prev) counter++;
+
+    prev = value;
+}
+
+Console.WriteLine("First part answer: " + counter);
+
+counter = 0;
+
+prev = Convert.ToInt32(inputData[0]) + Convert.ToInt32(inputData[1]) + Convert.ToInt32(inputData[2]);
+
+for (int i = 1; i < inputData.Length - 2; i++)
+{
+    int currentSum = Convert.ToInt32(inputData[i]) + Convert.ToInt32(inputData[i + 1]) + Convert.ToInt32(inputData[i + 2]);
+
+    if (currentSum > prev) counter++;
+
+    prev = currentSum;
+}
+
+Console.WriteLine("Second part answer: " + counter);
