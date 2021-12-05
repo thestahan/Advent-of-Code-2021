5779f8ecac8565979c0c1c30dba92aed30802fca
Kacper <thestahan@gmail.com>
2021-12-05 11:51:07 +0100

    Day 5 solution

---
+++ Advent of Code 2021/5_hydrothermal_venture/Point.cs
@@ -0,0 +1,20 @@
+﻿namespace _5_hydrothermal_venture
+{
+    internal record Point
+    {
+        public Point(string x, string y)
+        {
+            X = Int32.Parse(x);
+            Y = Int32.Parse(y);
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ Advent of Code 2021/5_hydrothermal_venture/Program.cs
@@ -0,0 +1,98 @@
+﻿using _5_hydrothermal_venture;
+
+var input = File.ReadAllLines("input.txt");
+
+List<(Point StartPoint, Point EndPoint)> points = new List<(Point, Point)>();
+
+foreach (var line in input)
+{
+    var (startPoint, endPoint) = GetTwoElementsSplittedByString(line, "->");
+
+    var (startPointX, startPointY) = GetTwoElementsSplittedByString(startPoint, ",");
+
+    var (endPointX, endPointY) = GetTwoElementsSplittedByString(endPoint, ",");
+
+    points.Add(new(new Point(startPointX, startPointY), new Point(endPointX, endPointY)));
+}
+
+var pointsOnMap = new List<Point>();
+
+foreach (var (StartPoint, EndPoint) in points)
+{
+    bool isHorizontalLine = StartPoint.Y == EndPoint.Y;
+
+    bool isVerticalLine = StartPoint.X == EndPoint.X;
+
+    if (isHorizontalLine)
+    {
+        int startX, endX;
+
+        if (StartPoint.X > EndPoint.X)
+        {
+            startX = EndPoint.X;
+            endX = StartPoint.X;
+        }
+        else
+        {
+            startX = StartPoint.X;
+            endX = EndPoint.X;
+        }
+
+        for (int i = startX; i <= endX; i++)
+        {
+            pointsOnMap.Add(new Point(i, StartPoint.Y));
+        }
+    }
+    else if (isVerticalLine)
+    {
+        int startY, endY;
+
+        if (StartPoint.Y > EndPoint.Y)
+        {
+            startY = EndPoint.Y;
+            endY = StartPoint.Y;
+        }
+        else
+        {
+            startY = StartPoint.Y;
+            endY= EndPoint.Y;
+        }
+
+        for (int i = startY; i <= endY; i++)
+        {
+            pointsOnMap.Add(new Point(StartPoint.X, i));
+        }
+    }
+    else // else statement is for part two
+    {
+        bool incrementX = StartPoint.X < EndPoint.X;
+
+        bool incrementY = StartPoint.Y < EndPoint.Y;
+
+        var (currentValueX, currentValueY) = (StartPoint.X, StartPoint.Y);
+
+        for (int i = 0; i <= Math.Abs(StartPoint.X - EndPoint.X); i++)
+        {
+            pointsOnMap.Add(new Point(currentValueX, currentValueY));
+
+            if (incrementX) currentValueX++;
+            else currentValueX--;
+
+            if (incrementY) currentValueY++;
+            else currentValueY--;
+        }
+    }
+}
+
+var groupedPoints = pointsOnMap.GroupBy(p => new { p.X, p.Y })
+    .Select(g => new { Value = g.Key, Count = g.Count() })
+    .OrderByDescending(x => x.Count);
+
+Console.WriteLine("Part two answer: " + groupedPoints.Where(g => g.Count > 1).Count());
+
+static (string, string) GetTwoElementsSplittedByString(string input, string splitter)
+{
+    var splittedInput = input.Split(splitter, StringSplitOptions.RemoveEmptyEntries);
+
+    return (splittedInput[0], splittedInput[1]);
+}
