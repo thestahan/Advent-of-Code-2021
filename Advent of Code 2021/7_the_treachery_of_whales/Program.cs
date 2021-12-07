5d40e73ebec87bd6dc613e66c54b800c2c6f474d
Kacper <thestahan@gmail.com>
2021-12-07 06:54:15 +0100

    Day 7 solution

---
+++ Advent of Code 2021/7_the_treachery_of_whales/Program.cs
@@ -0,0 +1,24 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+var crabPositions = input.First().Split(",").Select(Int32.Parse).ToList();
+
+var allPossibleCrabPositions = Enumerable.Range(0, crabPositions.Max());
+
+var partOneAnswer = allPossibleCrabPositions
+    .Select(c => crabPositions
+        .Sum(c2 => Math.Abs(c - c2)))
+    .Min();
+
+var partTwoAnswer = allPossibleCrabPositions
+    .Select(c => crabPositions
+        .Sum(c2 =>
+        {
+            int n = Math.Abs(c - c2);
+
+            return n * (n + 1) / 2;
+        }))
+    .Min();
+
+Console.WriteLine($"Part one answer: {partOneAnswer}");
+
+Console.WriteLine($"Part two answer: {partTwoAnswer}");
